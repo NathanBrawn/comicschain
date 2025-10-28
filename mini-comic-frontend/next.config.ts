ebda99fdafc14e7132ed191d1d746460189ec798
NathanBrawn <phuongnganphamphuong6@gmail.com>
2025-10-28 15:00:51 +0800

    fix basePath

--- mini-comic-frontend/next.config.ts
+++ mini-comic-frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/comicschain",
   reactStrictMode: true,
   trailingSlash: true,
   images: {
